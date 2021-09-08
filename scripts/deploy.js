4e523a534af253fb767ea7cc410c1ba33041f31a
hp <alinawaz1857@gmail.com>
2021-09-08 18:44:07 +0500

    test after 2021 07.05

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -3,10 +3,15 @@ const hre = require("hardhat");
 
 async function main() {
 
-    const tiki_tokens = await ethers.getContractFactory("TIKI");
-    const ngt = await tiki_tokens.deploy();
-    console.log("Tiki Tokens deployed to:", ngt.address);
+    // const tiki_tokens = await ethers.getContractFactory("TIKI");
+    // const ngt = await tiki_tokens.deploy();
+    // console.log("Tiki Tokens deployed to:", ngt.address);
 
+
+    const Greeter = await hre.ethers.getContractFactory("Greeter");
+    const greeter = await Greeter.deploy("Hello, Hardhat!");
+    await greeter.deployed();
+    console.log("Greeter deployed to:", greeter.address);
 }
 
 main()
