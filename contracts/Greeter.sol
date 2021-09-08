2807b728324fd0b5f8f5a699261e7f16659a86eb
hp <alinawaz1857@gmail.com>
2021-09-08 18:43:18 +0500

    test after 2021 07.00

--- contracts/Greeter.sol
+++ contracts/Greeter.sol
@@ -19,4 +19,8 @@ contract Greeter {
         console.log("Changing greeting from '%s' to '%s'", greeting, _greeting);
         greeting = _greeting;
     }
+ /*   event Deposit(address indexed _from, bytes32 indexed _id, uint _value);
+    function deposit(bytes32 _id) public payable {
+        emit Deposit(msg.sender, _id, msg.value);
+    }*/
 }
